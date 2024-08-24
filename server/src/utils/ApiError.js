20fe6c6c8e1bc653edc31cf193675fbe78018763
Gaurav Ghuge <guduughuge7@gmail.com>
2024-08-24 20:00:14 +0530

    update the modeling and code

---
+++ server/src/utils/ApiError.js
@@ -0,0 +1,26 @@
+
+class ApiError extends Error {
+
+   constructor(statusCode, message, data=null) {
+
+      super(message);
+
+      this.statusCode = statusCode;
+      this.data = data;
+      this.success = false;
+      
+
+      if(stack) {
+         this.stack =  stack
+      }
+      else {
+         Error.captureStackTrace(this, this.constructor);
+      }
+
+   }
+}
+
+
+export {
+   ApiError
+}
---
+++ server/src/utils/ApiResponse.js
@@ -0,0 +1,30 @@
+
+class ApiResponse {
+
+   constructor(status, message, data, anythingElse) {
+
+      this.status = status;
+      this.message = message;
+      this.data = data;
+      this.success = status < 400;
+      this.anythingElse = anythingElse;
+
+
+
+   }
+}
+
+export {
+   ApiResponse
+}
+
+
+/* 
+
+  .json(
+  
+     new ApiResponse(200, "employee registered successfully", employee)
+  )
+
+
+*/
